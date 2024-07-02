0cb6fbb9ccde1d28616b4dc2d54ce98bccf1b891
vtr57 <professorvitorfisica@gmail.com>
2024-07-02 11:01:56 -0300

    blog list e detail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(publicado_em__lte=timezone.now()).order_by('publicado_em') 
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
