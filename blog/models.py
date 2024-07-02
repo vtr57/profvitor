2af0a431bf1fac4500929d69c2eefa5ba539506d
vtr57 <professorvitorfisica@gmail.com>
2024-07-02 09:56:39 -0300

    add blog

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    criado_em = models.DateField(default=timezone.now)
+    publicado_em = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publicado_em = timezone.now()
+        self.save()
+
+    def __str__(self) -> str:
+        return self.titulo
